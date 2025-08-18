9edfa771060a325fb52481ee804c903883251c83
Shann <lagdaanshannarlly@gmail.com>
2025-08-18 19:16:48 +0800

    small changes sa time

--- lib/localDb.ts
+++ lib/localDb.ts
@@ -899,7 +899,6 @@ export const detectTimeManipulation = async (userEmail: string): Promise<{ isVal
   }
 };
 
-
 export const updateTimeSync = async (userEmail: string): Promise<void> => {
     try {
         await initDb();
